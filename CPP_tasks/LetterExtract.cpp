cc7e35d84fb6d0949d067f36e318925c55cfb609
Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>
2020-10-19 11:46:53 +0300

    Letters Extract fix Replaced list with set, used std functions to process file characters. Signed-off-by: Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>

--- CPP_tasks/LetterExtract.cpp
+++ CPP_tasks/LetterExtract.cpp
@@ -1,48 +1,33 @@
-#define CAP_ABC_START 'A'
-#define CAP_ABC_END 'Z'
-#define LOW_ABC_START 'a'
-#define LOW_ABC_END 'z'
-#define ABC_NUM_DIFF (LOW_ABC_START - CAP_ABC_START)
 #include <iostream>
 #include <fstream>
-#include <list>
+#include <set>
+#include <cctype>
 
 int main()
 {
     std::ifstream inFile;
     std::ofstream outFile;
-    std::list<char> letters;
+    std::set<char> letters;
     char fileChar;
-    bool uniqueCharFlag;
 
     inFile.open("task.in", std::fstream::in);
     outFile.open("task.out", std::ofstream::out);
 
     while (inFile.get(fileChar)) {
-        if (fileChar >= CAP_ABC_START && fileChar <= CAP_ABC_END || fileChar >= LOW_ABC_START && fileChar <= LOW_ABC_END) {
-            if (fileChar < LOW_ABC_START) {
-                fileChar += ABC_NUM_DIFF;
-            }
-
-            uniqueCharFlag = true;
-
-            for (char listLetter: letters) {
-                if (listLetter == fileChar) {
-                    uniqueCharFlag = false;
-                    break;
-                }
-            }
-
-            if (uniqueCharFlag) {
-                letters.push_back(fileChar);
-                outFile << fileChar;
-            }
+        if (std::isalpha(fileChar)) {
+            fileChar = std::tolower(fileChar);
+            letters.insert(fileChar);
         }
     }
 
-    letters.clear();
+    for (auto letter : letters) {
+        outFile << letter;
+    }
+    outFile << std::endl;
+
     inFile.close();
     outFile.close();
 
     return 0;
 }
+
