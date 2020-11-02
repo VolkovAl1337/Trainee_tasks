42bd4014ce47d806952db1f6ee652814a9eb1c7f
Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>
2020-11-02 19:08:09 +0200

    Kernel Module task fix 1 Refactor and function changes.
    -Pointers to null changed from 0 to NULL
    
    -Added open file opetaion
    
    -Changed order of includes
    
    -Changed work of rwite fucntion(improved performance)
    
    -Fixed excess lines and spacings
    
    Signed-off-by: Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>

--- Kernel_Modules/MessageModule.c
+++ Kernel_Modules/MessageModule.c
@@ -1,24 +1,31 @@
+#include <linux/cdev.h>
 #include <linux/debugfs.h>
+#include <linux/fcntl.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/fcntl.h>
 
 MODULE_LICENSE("GPL");
 
 #define MSG_SIZE 7
 #define BUF_SIZE 255
-#define FILE_NAME "test.txt"
+#define FILE_NAME "test"
 #define DIRECTORY_NAME "traineeTest"
 #define MSG " World\n"
 
+static size_t file_length= 0;
 static char foo_buf[BUF_SIZE];
-static struct dentry *dir = 0;
-static struct dentry *testFile = 0;
+static struct dentry *dir = NULL;
+static struct dentry *testFile = NULL;
+
+struct file_data {
+    struct cdev cdev;
+};
 
 static ssize_t file_read(struct file *file, char __user *buf, size_t count,
             loff_t *f_pos)
 {
-	return simple_read_from_buffer(buf, count, f_pos, foo_buf, sizeof(foo_buf));
+	return simple_read_from_buffer(buf, count, f_pos, foo_buf, BUF_SIZE);
 }
 
 static ssize_t file_write(struct file *file, const char __user *buf, size_t count,
@@ -26,77 +33,87 @@ static ssize_t file_write(struct file *file, const char __user *buf, size_t coun
 {
 	size_t ret;
 	int tmp = 0;
+	char* msg_pointer = MSG;
 
 	if (*f_pos > BUF_SIZE)
 		return -EINVAL;
 
-	ret = simple_write_to_buffer(foo_buf, sizeof(foo_buf), f_pos, buf, count);
+	ret = simple_write_to_buffer(foo_buf + file_length, BUF_SIZE, f_pos, buf, count);
 
 	if (ret < 0)
 		return ret;
 
-	--ret;
+	ret -= 1;
+	tmp = MSG_SIZE + ret;
 
-	while (tmp < MSG_SIZE) {
-		foo_buf[ret + tmp] = MSG[tmp];
-		++tmp;
+	while (ret < tmp) {
+		*(foo_buf + ret++) = *msg_pointer++;
 	}
 
-	ret += tmp;
-	foo_buf[ret] = '\0';
-
+	*(foo_buf + ret) = '\0';
 	return ret;
+}
+
+static int file_open(struct inode *inode, struct file *filep)
+{
+	struct file_data *my_data;
+	my_data = container_of(inode->i_cdev, struct file_data, cdev);
+	filep->private_data = my_data;
+	return 0;
 }
 
 static const struct file_operations file_fops = {
 	.owner = THIS_MODULE,
+	.open = file_open,
 	.read = file_read,
 	.write = file_write,
 };
 
-int create_file(void)
+void create_file(void)
 {
-	dir = debugfs_lookup(DIRECTORY_NAME, 0);
+	dir = debugfs_lookup(DIRECTORY_NAME, NULL);
 
 	if (!dir) {
-		dir = debugfs_create_dir(DIRECTORY_NAME, 0);
+		dir = debugfs_create_dir(DIRECTORY_NAME, NULL);
 
 		if (!dir) {
 			printk(KERN_ALERT "MessageModule: failed to create"
 				" /sys/kernel/debug/traineeTest\n");
-			return -1;
+			return;
 		}
 	}
+
 	testFile = debugfs_lookup(FILE_NAME, dir);
-	if(!testFile) {
-		testFile = debugfs_create_file(FILE_NAME, 0777, dir,
+
+	if (!testFile) {
+		testFile = debugfs_create_file(FILE_NAME, 0666, dir,
 		 "", &file_fops);
 
 		if (!testFile) {
 			printk(KERN_ALERT "MessageModule: failed to create"
-				" /sys/kernel/debug/traineeTest/test.txt\n");
-			return -1;
+				" /sys/kernel/debug/traineeTest/test\n");
+			return;
 		}
 	}
 
-	return 0;
+	return;
 }
 
 void delete_file(void)
 {
-	debugfs_remove(testFile);
 	debugfs_remove_recursive(dir);
 }
 
-
-static int msg_init(void)
+static int __init msg_init(void)
 {
 	create_file();
 	return 0;
 }
-static void msg_exit(void)
+
+static void __exit msg_exit(void)
 {
 	delete_file();
 }
+
 module_init(msg_init);
 module_exit(msg_exit);
